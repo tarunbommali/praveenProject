e55e9efa0a2acf89089c81166fa50d0271d2a59e
Praveen0117 <polakipraveenkumar9@gmail.com>
2024-04-09 22:16:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import react from "react"
+import ReactDOM from "react-dom"
+
+
+const App = () => {
+    return(
+        <div>
+            <h1>Praveen</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
